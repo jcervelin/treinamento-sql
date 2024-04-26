11efe7ee03947c2421e7cc0e25ab784b46b20455
Juliano Cervelin <juliano.cervelin@gmail.com>
2024-04-26 21:14:41 +0100

    Aula 5

--- Aula5/Constraints - Chaves estrangeiras.sql
+++ Aula5/Constraints - Chaves estrangeiras.sql
@@ -1,3 +1,14 @@
+INSERT INTO Shows (Id_Banda, Id_Palco, Data, Hora, Preco) VALUES (100, 300, '2050-07-15', '10:00', 150.00);
+select * from shows;
+
+delete from shows where id=8;
+
+SELECT b.nome Banda, p.nome_palco Palco, s."data" , s.preco 
+FROM Shows s
+	INNER JOIN Bandas b ON s.Id_Banda = b.Id
+	INNER JOIN Palcos p ON s.Id_Palco = p.id_palco
+where s.preco > 100;
+
 --Criando Chaves Estrangeiras e Constraints
 --Adicionar Chave Estrangeira
 --Para garantir que os IDs de bandas e palcos nos shows correspondam a entradas válidas nas tabelas Bandas e Palcos, você pode adicionar chaves estrangeiras à tabela Shows.
